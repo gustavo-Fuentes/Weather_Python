f74929f75f55ff741bd0854ec11ab8816144ad45
Gustavo Fuentes <fuentesfei99@gmail.com>
2021-04-02 17:07:40 -0300

    Muda a ordem do dia da semana conforme a interface

--- app/service/api_service.py
+++ app/service/api_service.py
@@ -1,16 +1,19 @@
 
 import requests
 import os
-
+from datetime import datetime, time
+import pytz
 
 APPID = os.environ["APPID"]
+
 
 class OpenWeatherMap:
     def __init__(self) -> None:
         self.baseUrl = "https://api.openweathermap.org/data/2.5/"
 
-    def getForecast(self, city):
+    def getForecast(self, city, sort=True):
         coord = self.getLocation(city)
+        
         params = {
             "lat": coord["lat"],
             "lon": coord["lon"],
@@ -19,9 +22,22 @@ class OpenWeatherMap:
             "lang": "pt_br",
             "units": "metric",
         }
+        
         url = self.baseUrl + "onecall"
-        response = requests.get(url, params=params)
-        return response.json()
+        response = requests.get(url, params=params).json()
+        
+        if sort:
+            timezone = pytz.timezone(response["timezone"])
+            response["daily"].pop()
+            for index,forecastDay in enumerate(response["daily"]):
+                localTime = datetime.fromtimestamp(forecastDay["dt"],timezone)
+                
+                weekDay = localTime.strftime("%w")
+                response["daily"][index]["weekday"] = int(weekDay)
+            
+            response["daily"].sort( key=lambda x: x["weekday"])
+            
+        return response
     
     def getLocation(self, city):
         params = {
